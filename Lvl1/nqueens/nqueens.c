3d1f052a59ae9f26797adde2d4d6790393bc9f65
Bendix Schmidt <bendixschmidt@MBP-von-Bendix.fritz.box>
2024-11-14 17:38:00 +0100

    First task

---
+++ Lvl1/nqueens/nqueens.c
@@ -0,0 +1,96 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nqueens.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bendixschmidt <bendixschmidt@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:38:55 by bendixschmi       #+#    #+#             */
+/*   Updated: 2024/11/14 17:30:12 by bendixschmi      ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+/*
+Given a number n, you have to place n queens on a chess board with the size n*n
+while the queens cannot attack each other.
+
+You have to print all the different solutions in the following format:
+1 3 0 2
+2 0 3 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int	safeQ(int *arr, int col, int row)
+{
+	int i = 0;
+
+	while (i < col)
+	{
+		if (arr[i] == row)
+			return (0);
+		if (arr[i] - i == row - col)
+			return (0);
+		if (arr[i] + i == row + col)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	print_sol(int *arr, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == 0)
+			printf("%d", arr[i]);
+		else
+			printf(" %d", arr[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+void	solveNQ(int *arr, int n, int col)
+{
+	int i = 0;
+
+	if (col == n)
+	{
+		print_sol(arr, n);
+		return ;
+	}
+	while (i < n)
+	{
+		if (safeQ(arr, col, i))
+		{
+			arr[col] = i;
+			solveNQ(arr, n, col + 1);
+		}
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("wrong amount of arguments!\n");
+		return (0);
+	}
+	else
+	{
+		int n = atoi(argv[1]);
+		if (n < 1)
+			return (0);
+		int arr[n];
+		solveNQ(arr, n, 0);
+	}
+	return (0);
+}
